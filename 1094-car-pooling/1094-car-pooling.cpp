ac3f12b169b0bc639c0e15ec3b83556405ce9752
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-01-08 17:19:15 +0530

    Time: 11 ms (86.98%), Space: 9.7 MB (94.35%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-     int in_car = 0;
-        int increase[1001] = {0};
-        for (int i = 0; i < trips.size(); i ++) { // find out the number of the **net increase** passengers for each stop
-            increase[trips[i][1]] += trips[i][0];
-            increase[trips[i][2]] -= trips[i][0];
+        int people=0;
+        int stops[1001]={0};
+        for(int i=0;i<trips.size();i++){
+            stops[trips[i][1]]+=trips[i][0];
+            stops[trips[i][2]]-=trips[i][0];
         }
-        for (int i = 0; i <= 1000; i ++) { // from start to end, for each stop we calculate the number of passengers in the car.
-            in_car += increase[i];
-            if (in_car > capacity) return false; // once the number of passengers in the car exceeds the capacity
+        for(int i=0;i<1001;i++){
+            people+=stops[i];
+            if(people>capacity)
+                return false;
         }
         return true;
     }
