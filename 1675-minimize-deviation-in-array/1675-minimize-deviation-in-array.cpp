b5addfccd285804f791773279e6066b6927e9930
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-02-20 03:01:22 +0530

    Time: 901 ms (7.28%), Space: 121.3 MB (10.60%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumDeviation(vector<int>& nums) {
+        set <int>  s;
+        for(int i = 0; i<nums.size() ; ++i)
+        {
+            if(nums[i] % 2 == 0)
+                s.insert(nums[i]);
+            
+            else
+                // Odd number are transformed
+                // using 2nd operation
+                s.insert(nums[i] * 2);
+        }
+        int diff = *s.rbegin() - *s.begin();
+        
+        //run the loop untill difference is minimized
+        while(*s.rbegin() % 2 == 0)
+        {
+            
+            // Maximum element of the set
+            int x = *s.rbegin();
+            s.erase(x);
+            // remove begin element and inserted half of it for minimizing
+            s.insert(x/2);
+            
+            diff = min(diff, *s.rbegin() - *s.begin());
+        }
+        return diff;
+    }
+};
