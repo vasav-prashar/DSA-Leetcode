c22de1a8278987d0842f55c8948082e20b5b378b
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-04-12 22:02:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.61%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  long mod = 1000000007;
+ public: int countOrders(int n) {
+    if(n == 0) return 1;
+    long ans = n*(2*n-1) % mod;
+    ans = ans * countOrders(n-1) % mod;
+    return (int)ans;
+  }
+};
