2b409c679798a7ef2cbda892c5255a387c5683cd
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-05-16 23:24:46 +0530

    Time: 455 ms (11.15%), Space: 92.6 MB (5.07%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0]==1||grid[grid.size()-1][grid[0].size()-1])
+        vector <vector <int>> moves = {{-1,-1}, {-1, 0}, {-1, 1}, {0, -1},{0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        int n = grid.size() - 1;
+        int m = grid[0].size() - 1;
+        queue <vector <int>> que;
+        if(grid[0][0] || grid[n][m])
             return -1;
-        
-        vector<vector<int>> dir={{-1,-1}, {-1, 0}, {-1, 1}, {0, -1},{0, 1}, {1, -1}, {1, 0}, {1, 1}};
-        int n=grid.size();
-        int m=grid[0].size();
-        if(grid[0][0]==0&&m==1&&n==1) return 1;
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        queue<vector<int>> q;
-        int dist;
-        q.push({0,0,1});
-        vis[0][0]=1;
-        while(!q.empty()){
-            vector<int> f=q.front();
-            int x=f[0],y=f[1],dist=f[2];
-            cout<<f[0]<<f[1]<<f[2]<<endl;
-            q.pop();
-            if(f[0]==n-1&&f[1]==m-1)
-                return f[2];
-            
-            for(vector<int> move:dir){
-                int newx=f[0]+move[0];
-                int newy=f[1]+move[1];
-                
-                if(newx>=0&&newx<n&&newy>=0&&newy<m&&grid[newx][newy]==0&&vis[newx][newy]==0){
-                    q.push({newx,newy,f[2]+1});
-                    vis[newx][newy]=1;
-                    
+        que.push({0, 0, 1});
+        while(!que.empty()){
+            vector <int> top = que.front();
+            que.pop();
+            if(top[0] == n && top[1] == m)
+                return top[2];
+            for(vector <int> move: moves){
+                int x = top[0] + move[0], y = top[1] + move[1];
+                if( 0 <= x && x <= n && 0 <= y && y <= m && grid[x][y] == 0){
+                    grid[x][y] = 1;  // mark visted
+                    que.push({x, y, top[2] + 1});
                 }
-                
-            } 
+            }
         }
         return -1;
     }
