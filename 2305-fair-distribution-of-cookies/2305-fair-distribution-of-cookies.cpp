c537b4de5ed5912e608797ce6c7a9716171a6093
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-07-12 21:38:45 +0530

    Time: 20 ms (82.61%), Space: 8.9 MB (6.76%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    
-    bool helper(vector<int> &cookies,int limit,vector<int>&v,int index){
-        bool ans=false;
-        if(index>=cookies.size())
-            return true;
-        for(int i=0;i<v.size();i++){
-            if(cookies[index]+v[i]<=limit){
-                v[i]+=cookies[index];
-            ans=ans||helper(cookies,limit,v,index+1);
-            v[i]-=cookies[index];
-            }
+    void helper(vector<int> cookies,int idx,int &ans,int k,vector<int> &bag){
+        if(idx==cookies.size()){
+            int largest=0;
+            for(int i=0;i<bag.size();i++)
+                largest=max(largest,bag[i]);
+            ans=min(ans,largest);
+            return;
         }
-        return ans;
+            for(int i=0;i<k;i++){
+                bag[i]+=cookies[idx];
+                helper(cookies,idx+1,ans,k,bag);
+                bag[i]-=cookies[idx];
+                if(bag[i]==0)
+                    break;
+            }
+        
+        
     }
-    
     int distributeCookies(vector<int>& cookies, int k) {
-        sort(cookies.begin(),cookies.end());
-        int n=cookies.size();
-        int i=*max_element(begin(cookies),end(cookies)),j=accumulate(begin(cookies),end(cookies),0);
-        while(i<=j){
-            int mid=i+((j-i)/2);
-            vector<int> v(k,0);
-            if(helper(cookies,mid,v,0)){
-                j=mid-1;
-            }
-            else
-                i=mid+1;
-        }
-        return i;
+       int n=cookies.size();
+        vector<int> bag(k,0);
+        int ans=INT_MAX;
+        helper(cookies,0,ans,k,bag);
+        return ans;
     }
 };
