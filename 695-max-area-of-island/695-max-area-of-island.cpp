b896273b9897c94ffc117b3480f2d814c139fc67
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-07-15 20:20:03 +0530

    Time: 36 ms (35.70%), Space: 23.1 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
+    // we use dfs and after visiting the cell mark it as 0 so ourhelper function will not take the value one more time
+    int helper(vector<vector<int>> &grid,int i,int j){
+        if(i>=0&&i<grid.size()&&j>=0&&j<grid[0].size()&&grid[i][j]==1){
+            grid[i][j]=0;
+            return 1+helper(grid,i+1,j)+helper(grid,i-1,j)+helper(grid,i,j+1)+helper(grid,i,j-1);
+        }
+        else return 0;
+    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxarea=0;
-        int row=grid.size();
-        int col=grid[0].size();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                // if(grid[i][j]==1)
-                maxarea=max(maxarea,area(i,j,grid));
+        int ar=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1)
+                ar=max(ar,helper(grid,i,j));
             }
         }
-        return maxarea;
-    }
-    int area(int i,int j,vector<vector<int>>& grid){
-        if(i<0||i>=grid.size()||j<0||j>=grid[0].size()||grid[i][j]==0){
-            return 0;
-        }
-        else{
-            grid[i][j]=0;
-            return (1+area(i+1,j,grid)+area(i-1,j,grid)+area(i,j+1,grid)+area(i,j-1,grid));
-        }
+        return ar;
     }
 };
