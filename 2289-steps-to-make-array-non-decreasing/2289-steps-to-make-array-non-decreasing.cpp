ed64e59da6f2b2d05e4e100299d429805a90f489
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-07-17 00:12:29 +0530

    Time: 270 ms (44.85%), Space: 76.4 MB (68.78%) - LeetHub

--- 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.cpp
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.cpp
@@ -3,24 +3,22 @@ public:
     // use stack of pair with element and steps to remove and we traverse the array form right to left
     //https://leetcode.com/problems/steps-to-make-array-non-decreasing/discuss/2290383/Detailed-and-Intuitive-explanation
     int totalSteps(vector<int>& nums) {
-         stack<pair<int, int>> stk;
-    
-    int cnt = 0;
-    int res = 0;
-    
-    for(int i = nums.size() - 1;i >= 0;i--) {
-    
-        cnt = 0;
-        
-        while(!stk.empty() && nums[i] > stk.top().first) {
-            cnt = max(cnt+1, stk.top().second);
-            stk.pop();
+        stack<pair<int,int>> st;
+        int n=nums.size();
+        int steps=0;
+        int currSteps=0;
+        for(int i=n-1;i>=0;i--){
+            int currSteps=0;
+           
+            while(!st.empty()&&nums[i]>st.top().first){
+                // currSteps++;
+                currSteps=max(currSteps+1,st.top().second);
+                st.pop();
+                
+            }
+            st.push({nums[i],currSteps});
+            steps=max(steps,currSteps);
         }
-        res = max(res, cnt);
-        stk.push({nums[i], cnt});
-        
-    }
-    return res;
-
+        return steps;
     }
 };
