2498842358be856400a787298db5cd43dd58896d
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-07-20 00:08:38 +0530

    Time: 3 ms (54.07%), Space: 7.8 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
+    //recursive approach
+//     int helper(int index,vector<int> &nums){
+//         if(index==0)
+//             return nums[index];
+//         if(index<0)
+//             return 0;
+//         int pick=nums[index]+helper(index-2,nums);
+//         int notPick=0+helper(index-1,nums);
+//         return max(pick,notPick);
+            
+//     }
+    //top-down memoization
+        int helper(int index,vector<int> &nums,vector<int> &dp){
+        if(index==0)
+            return nums[index];
+        if(index<0)
+            return 0;
+        if(dp[index]!=-1)
+            return dp[index];
+        int pick=nums[index]+helper(index-2,nums,dp);
+        int notPick=0+helper(index-1,nums,dp);
+        dp[index]=max(pick,notPick);
+        return dp[index];
+            
+    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,0);
-        dp[0]=0;
-        dp[1]=nums[0];
-        for(int i=1;i<nums.size();i++){
-            dp[i+1]=max(dp[i],dp[i-1]+nums[i]);
-        }
-        return dp[nums.size()];
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return helper(n-1,nums,dp);
     }
 };
