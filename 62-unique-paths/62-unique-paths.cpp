a41c1bb87a779e30fcd3e04cff06e25782f82054
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-07-28 00:03:16 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (44.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -12,19 +12,36 @@ public:
     //     int left=recur(i,j-1);
     //     return ans=up+left;
     // }
-    int topDown(vector<vector<int>> &dp,int m,int n){
-        if(m-1==0&&n-1==0)
-            return 1;
-        if(m-1<0||n-1<0)
-           return 0;
-        if(dp[m-1][n-1]!=-1)
-            return dp[m-1][n-1];
-        int up=topDown(dp,m-1,n);
-        int left=topDown(dp,m,n-1);
-        return dp[m-1][n-1]=up+left;
-    }
+    // memoization TopDown approach
+    // int topDown(vector<vector<int>> &dp,int m,int n){
+    //     if(m-1==0&&n-1==0)
+    //         return 1;
+    //     if(m-1<0||n-1<0)
+    //        return 0;
+    //     if(dp[m-1][n-1]!=-1)
+    //         return dp[m-1][n-1];
+    //     int up=topDown(dp,m-1,n);
+    //     int left=topDown(dp,m,n-1);
+    //     return dp[m-1][n-1]=up+left;
+    // }
+    
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return topDown(dp,m,n);
+        vector<vector<int>> dp(m,vector<int>(n,1));
+        dp[0][0]=1;
+       
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                
+               
+                
+                int up=dp[i-1][j];
+              
+                int left=dp[i][j-1];
+                
+                dp[i][j]=up+left;
+                
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
