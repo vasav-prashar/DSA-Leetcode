3b32f27b94e4c3aa83337cc1395aee127f6c4d9c
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-08-03 01:36:25 +0530

    Time: 20 ms (99.81%), Space: 13.1 MB (92.86%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        vector<int> val;
+    // using binary search
+    int countLessThanMid(vector<vector<int>> &matrix,int mid){
+        int count=0;
+        int i=0,j=matrix.size()-1;//first col and last col
         for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                val.push_back(matrix[i][j]);
-            }
+            while(j>=0&&matrix[i][j]>mid)// if elemmnt is greater than mid then we neglect the col
+                j--;
+            count+=j+1;
+            
         }
-        sort(val.begin(),val.end());
-        return val[k-1];
+        return count;
+    }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int l=matrix[0][0];
+        int h=matrix[matrix.size()-1][matrix.size()-1];
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(countLessThanMid(matrix,mid)<k)// check the lesser or equal to k elements count
+               l=mid+1;
+               else
+               h=mid-1;
+            
+        }
+        return l;
     }
 };
