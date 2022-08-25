0c16404bbb3264112a19e1927dfb557aba7922f5
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-08-25 12:58:25 +0530

    Time: 58 ms (9.51%), Space: 9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        vector<int> a(26,0);
+        map<int,int> m1;
+        
         for(int i=0;i<magazine.size();i++){
-            a[magazine[i]-'a']++;
+            m1[magazine[i]]++;
         }
         for(int i=0;i<ransomNote.size();i++){
-            if(--a[ransomNote[i]-'a']<0)
+            if(m1[ransomNote[i]]>0)
+                m1[ransomNote[i]]--;
+            else
                 return false;
         }
         return true;
