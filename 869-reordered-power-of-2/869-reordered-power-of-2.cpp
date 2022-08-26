d3678491c2e2a63b833a8aaef978b8cf7e2f6dae
Vasav Prashar <74191996+vasav-prashar@users.noreply.github.com>
2022-08-26 20:25:21 +0530

    Time: 7 ms (42.94%), Space: 5.9 MB (83.90%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        string s=to_string(n);
+        sort(s.begin(),s.end());
+        for(int i=0;i<32;i++){
+            string temp=to_string(1<<i);//left shift 1 bit means pow(2,i)
+            sort(temp.begin(),temp.end());
+            if(s==temp)
+                return true;
+            
+        }
+        return false;
+    }
+};
